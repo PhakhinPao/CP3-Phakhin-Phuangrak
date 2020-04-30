f1af7d742b934b7c0fe30742e19d4c607e8e536f
PhakhinPao <bigpao2000@gmail.com>
2020-04-30 19:53:58 +0700

    Create Exercise_4_Phakhin_P.py

---
+++ Exercise_4_Phakhin_P.py
@@ -0,0 +1,11 @@
+# for variable
+FE = 89
+GB = 87
+ICS = 86
+CP = 85
+# for function
+print("--- Your Score ---")
+print("Foundation English : " + str(FE) + " คะแนน")
+print("General Business : " + str(GB) + " คะแนน")
+print("Introduction to Computer Systems : " + str(ICS) + " คะแนน")
+print("Computer Programming : " + str(CP) + " คะแนน")
